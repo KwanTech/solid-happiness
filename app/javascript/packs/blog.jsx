841f222e96e145cdca834251cbcb29f88a1dd093
Lionel E. Ramos <ltrainpr@gmail.com>
2017-07-29 10:48:15 -0600

    Go to next blog

--- app/javascript/packs/blog.jsx
+++ app/javascript/packs/blog.jsx
@@ -1,16 +1,14 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 function Blog(props) {
   return (
-    <div>
+    <div className="blog">
+      <h1 className='title'>{props.blog.title}</h1>
       <p>{props.blog.body}</p>
-      <a href="#" className="back-to-posts">Back</a>
+      <strong><a href="#" className="back-to-posts">Return</a></strong>
+      <strong><a href="#" className="next-blog" data-post-id={props.blog.nextBlogId}>Next</a></strong>
     </div>
   )
 }
@@ -25,21 +23,36 @@ document.addEventListener('DOMContentLoaded', () => {
     ev.preventDefault();
     let readMe = ev.target.className;
 
-    if (readMe === 'read-more') {
-      let post = getPost(data, ev.target.getAttribute('data-post-id'))
-      ReactDOM.render(
-        <Blog blog={post} />,
-        ev.currentTarget,
-      )
-    } else if (readMe === "back-to-posts") {
-      postsGrid.innerHTML = postsHtml;
-    };
+    switch(readMe) {
+      case 'read-more':
+        let post = getPost(data, ev.target.getAttribute('data-post-id'));
+        renderBlog(post, ev.currentTarget);
+        break;
+      case "back-to-posts":
+        postsGrid.innerHTML = postsHtml;
+        break;
+      case "next-blog":
+        let nextPostId = ev.target.getAttribute('data-post-id');
+        let nextPost = getPost(data, nextPostId);
+        renderBlog(nextPost, postsGrid);
+        break;
+      default:
+        return;
+    }
   });
 })
 
+function renderBlog (blog, container) {
+  ReactDOM.render(
+    <Blog blog={blog} />,
+    container,
+  )
+}
+
 function getPost(posts, postId) {
-  for(var postObject of posts) {
+  for(let [index, postObject] of posts.entries()) {
     if (postObject["id"].toString() === postId) {
+      postObject["nextBlogId"] = posts[index + 1]["id"]
       return postObject
     }
   }
