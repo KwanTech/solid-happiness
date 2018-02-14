088af1d9ef34002f5931b77d2fa92c60b76dcbb3
Lionel E. Ramos <ltrainpr@gmail.com>
2018-02-14 06:42:47 -0700

    Change from register/people page to guidance page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get '/why', to: 'why#index'
-  get '/register', to: 'people#index'
+  get '/guidance', to: 'guidance#index'
   get '/blog', to: 'blog#index'
   resources :people, only: [:index, :create]
 end
