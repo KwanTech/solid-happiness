e1105b14b76514ee250baa3dc316144bd6e2cee3
Lionel E. Ramos <ltrainpr@gmail.com>
2018-02-18 17:42:00 -0700

    Remove guidance route/page.  Just use blog page that's already there

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get '/why', to: 'why#index'
-  get '/guidance', to: 'guidance#index'
   get '/blog', to: 'blog#index'
   get '/exercises', to: 'exercises#index'
 end
